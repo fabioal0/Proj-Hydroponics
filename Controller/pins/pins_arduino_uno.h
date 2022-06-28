25f57dc05d83850e6991695e53e6242e6256e062
fabioal0 <fabioal0@hotmail.com>
2022-06-28 23:43:44 +0100

    UPDATE (pins) Change the pins and remove some of them

--- Controller/pins/pins_arduino_uno.h
+++ Controller/pins/pins_arduino_uno.h
@@ -4,52 +4,53 @@
  *  Arduino Uno pin assignments
  */
 
-#if !defined(__AVR_ATmega328P__) || !defined(__AVR_ATmega168__) 
-#error "Select Arduino Uno in 'Tools -> Board'."
-#endif
-
 //
 // Buttons
 //
-#define ENTER_BUTTON_PIN                      13
-#define DOWN_BUTTON_PIN                       12
-#define UP_BUTTON_PIN                         11
+#define ENTER_BUTTON_PIN                      4
+#define DOWN_BUTTON_PIN                       6
+#define UP_BUTTON_PIN                         7
+
+//
+// Potenciometers
+//
+#define MAIN_POTENCIOMETER_PIN                A1  // Analog Input
 
 //
 // Pumps
 //
-#define NUTRIENTS_PUMP_SPEED_PIN              A0  // Analog Output
-#define WATER_PUMP_SPEED_PIN                  A1  // Analog Output
+//#define NUTRIENTS_PUMP_SPEED_PIN              
+#define WATER_PUMP_SPEED_PIN                  5   // PWM
 
 //
 // Sensors
 //
-#define TEMPERATURE_SENSOR_PIN                A2  // Analog Input
+#define TEMPERATURE_SENSOR_PIN                A0  // Analog Input
 
 //
 // Heater
 //
-#define WATER_HEATER_PIN                      10  
+#define WATER_HEATER_STATE_PIN                11  
 
 //
 // Light
 //
-#define LIGHT_STATE_PIN                       9
+#define LIGHT_STATE_PIN                       10  
 
 //
 // Display
 //
-#define E0_DISPLAY_PIN                        8
-#define E1_DISPLAY_PIN                        7
-#define E2_DISPLAY_PIN                        6
-#define E3_DISPLAY_PIN                        5
+//#define E0_DISPLAY_PIN                        
+//#define E1_DISPLAY_PIN                        
+//#define E2_DISPLAY_PIN                        
+//#define E3_DISPLAY_PIN                        
 
-#define LATCH_DISPLAY_PIN                     4
-#define DATA_DISPLAY_PIN                      3
-#define CLK_DISPLAY_PIN                       2
+//#define LATCH_DISPLAY_PIN                     
+//#define DATA_DISPLAY_PIN                      
+//#define CLK_DISPLAY_PIN                       
 
 //
 // Wifi
 //
-#define TX_PIN                                1 
-#define RX_PIN                                0
+//#define TX_PIN                                
+//#define RX_PIN                                
